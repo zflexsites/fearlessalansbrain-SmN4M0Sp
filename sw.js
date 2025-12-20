53b6a157042eb918f4408a854b94c75cadfbcaaa
fearlessalan <fearlessalain@gmail.com>
2025-12-20 13:08:08 +0200

    chore(build): zFlex auto build 2025-12-20T11:08:00.288Z

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const OFFLINE_URL = "offline.html";
 const CORE_ASSETS = [
   OFFLINE_URL,
   "/assets/app-CGcfMqaW.css",
-  "/assets/app-bft3p-vM.js",
+  "/assets/app-BIDZchwr.js",
   "/manifest.json", // Essentiel pour l'expérience PWA
   // On pourrait ajouter ici le logo principal ou des icônes critiques.
 ];
